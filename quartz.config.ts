185971d7702371179f0cc3d9973eb317944fd451
猎头，科技行业人才专家 | 个人生产力系统达人 <chris.pxk@gmail.com>
2026-05-07 17:10:50 +0800

    google search console html验证

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,6 +17,15 @@ const config: QuartzConfig = {
       provider: "google",
       tagId: "G-MSZMTLM3JH",
     },
+    head: [
+      {
+        tag: "meta",
+        attrs: {
+          name: "google-site-verification",
+          content: "<meta name="google-site-verification" content="78S06te19_l5moOf3u7fL6UW5CkqqNgSKJwyZfQ4cu4" />",  // 替换成从 Google 复制的内容
+        },
+      },
+    ],
     locale: "zh-CN",
     baseUrl: "ichris007.github.io",
     ignorePatterns: ["Private", "Templates", ".obsidian"],
