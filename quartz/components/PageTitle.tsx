0431f1d20b6a310aecd004a78e603c1a2d4b9c36
猎头，科技行业人才专家 | 个人生产力系统达人 <chris.pxk@gmail.com>
2026-05-08 03:32:19 +0800

    updated PageTitle.tsx title logo

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -8,7 +8,14 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>
+        <img 
+          src="/static/icon.png" 
+          alt="Logo" 
+          class="page-title-logo"
+        />
+        {title}
+      </a>
     </h2>
   )
 }
@@ -18,6 +25,18 @@ PageTitle.css = `
   font-size: 1.75rem;
   margin: 0;
   font-family: var(--titleFont);
+}
+
+.page-title a {
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+  text-decoration: none;
+}
+
+.page-title-logo {
+  height: 2rem;
+  width: auto;
 }
 `
 
